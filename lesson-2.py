7ce6abd42a8bfa283bc553ecfc20d147366e2843
Andrea Lauricella <andrealauricella97@gmail.com>
2025-04-19 22:24:13 +0200

    refactor: support image via local file with base64 encoding

--- lesson-2.py
+++ lesson-2.py
@@ -1,6 +1,18 @@
 from openai import OpenAI
+import base64
+
 from dotenv import load_dotenv # In questo modo carichiamo le variabili d'ambiente dal file .env per maggiore sicurezza
 load_dotenv()
+
+# Funzione per convertire un'immagine in una stringa base64
+# Questo è necessario per poter inviare l'immagine tramite API come testo codificato
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+    
+# Codifica l'immagine specificata e la salva nella variabile base64_image    
+base64_image = encode_image("./image.jpg")
+
 
 client = OpenAI()
 
@@ -14,13 +26,13 @@ input_messages = [
             },
             {
                 "type": "input_image",
-                "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Ginger_european_cat.jpg/250px-Ginger_european_cat.jpg"
-            } # URL di un'immagine di un gatto, in output ci aspettiamo una descrizione dell'immagine
+                "image_url": f"data:image/jpeg;base64,{base64_image}"
+            } 
         ]
     }
 ]
 
-response = client.responses.create(
+response = client.responses.create( 
     model="gpt-4o-mini",
     input=input_messages,
 )
