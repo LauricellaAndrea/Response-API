3e34d1624be0c979571fa019b6edf891c12d29f4
Andrea Lauricella <andrealauricella97@gmail.com>
2025-04-19 18:25:20 +0200

    Creato response message, passato la chiave tramite env, aggiunto un system message basic, impostata la streaming responses

---
+++ lesson-1.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+from dotenv import load_dotenv # In questo modo carichiamo le variabili d'ambiente dal file .env per maggiore sicurezza
+load_dotenv()
+
+client = OpenAI()
+
+input_messages = [
+    {
+        "role": "assistant",
+        "content": "Perchè il cielo è blu?"
+    }
+]
+
+response = client.responses.create(
+    model="gpt-4o-mini",
+    instructions="rispondi come un pirata", 
+    input=input_messages,
+    stream=True, #Con streaming possiamo ricevere i risultati in tempo reale
+)
+
+#print(response.output_text)
+
+# With Stream 
+
+full_response = ""
+
+for event in response:
+    if event.type == "response.output_text.delta":
+        print(event.delta, end="", flush=True) # end="" serve per non andare a capo e flush=True serve per forzare l'output
+        full_response += event.delta
+
+print("\n\n\nFull reponse:", full_response) # aggiungiamo nnnn per andare a capo e stampiamo la risposta completa
+
+#In questo modo possiamo vedere il risultato in tempo reale, mentre il modello sta generando la risposta, come accade con ChatGPT
