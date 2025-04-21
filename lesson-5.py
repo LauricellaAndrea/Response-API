8053dc80985e7570b80f666e09ebbdd1243509be
Andrea Lauricella <andrealauricella97@gmail.com>
2025-04-22 01:06:24 +0200

    aggiunta user location

--- lesson-5.py
+++ lesson-5.py
@@ -8,18 +8,25 @@ input_messages = [
  
  {
      "role": "user",
-     "content": "Qual è il meteo di oggi a nardò?"
+     "content": "Qual è il meteo di oggi da me?"
  }
 ]
 
 tools = [
     {
-        "type": "web_search_preview" # questo è lo strumento datoci da OpenAI per fare ricerche sul web con response api
+        "type": "web_search_preview", # questo è lo strumento datoci da OpenAI per fare ricerche sul web con response api
+        "user_location" : { # questo è il campo che ci permette di definire la posizione dell'utente
+            "type": "approximate",
+            "country": "IT",
+            "city": "Nardò",
+            "region": "Puglia"
+        }
     }
 ]
 
 response = client.responses.create(
     model="gpt-4o-mini",
+    instructions="Please provide a weather forecast for the user's location.", # tramite un system prompt possiamo definire il comportamento del modello
     input=input_messages,
     tools= tools,
 )
